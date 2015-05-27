7ab5277c5d18817d9e13d31af38a8aad0d7ce993
zuoshifan <zuoshifan@163.com>
2015-05-27 16:49:49 -0400

    Some new work on polarizations dirty map

--- map/dirty_map.py
+++ map/dirty_map.py
@@ -626,6 +626,8 @@ class DirtyMapMaker(object):
                     mpiutil.world.Recv(U_noise_mat, source=2, tag=0)
                     print 'Process 1 has received U noise matrix from 2.'
                     QU_noise_matrix[1, :, :, 1, :, :] = U_noise_mat # U noise matrix
+                    del noise_mat
+                    del U_noise_mat
                     QU_noise_inv = QU_noise_matrix.inv()
                     del QU_noise_matrix
                     # construct a new large vector to hold the Q, U time stream
@@ -637,37 +639,56 @@ class DirtyMapMaker(object):
                     mpiutil.world.Recv(U_time_stream, source=2, tag=1)
                     print 'Process 1 has received U time stream from 2.'
                     QU_time_stream[1] = U_time_stream
+                    del time_stream
+                    del U_time_stream
                     # # calculate N^-1 d
                     # Ninvd = al.dot(QU_noise_inv, QU_time_stream)
                     # print Ninvd.shape
                     # calculate P^T N^-1
                     PTNinv = al.partial_dot(pointing_mat.mat_transpose(), QU_noise_inv)
                     QU_map = al.dot(PTNinv, QU_time_stream)
-                    print 'Process %d: ' % mpiutil.rank, QU_map.shape
-                    print 'Process %d: ' % mpiutil.rank, QU_map.info
                     map += QU_map[:, :, 0, :].transpose((2, 0, 1)) # Q dirty map
                     U_map = QU_map[:, :, 1, :].transpose((2, 0, 1)) # U dirty map
-                    mpiutil.world.Send(np.ascontiguousarray(U_map), dest=2, tag=3)
+                    U_map = np.ascontiguousarray(U_map)
+                    del QU_map
+                    mpiutil.world.Send(U_map, dest=2, tag=3)
+                    del U_map
                     print 'Process 1 has send U dirty map to 1.'
+
+                    PTNinvP = al.partial_dot(PTNinv, pointing_mat)
+                    print 'Process %d: ' % mpiutil.rank, PTNinvP.shape
+                    print 'Process %d: ' % mpiutil.rank, PTNinvP.info
                 elif mpiutil.rank == 2:
+                    del pointing_mat # Not used by this process
                     mpiutil.world.Send(noise_mat, dest=1, tag=0)
+                    del noise_mat
                     print 'Process 2 has send U noise matrix to 1.'
                     mpiutil.world.Send(time_stream, dest=1, tag=1)
+                    del time_stream
                     print 'Process 2 has send U time stream to 1.'
                     U_map = al.empty_like(map)
                     mpiutil.world.Recv(U_map, source=1, tag=3)
                     print 'Process 2 has received U dirty map from 2.'
                     map += U_map
+                    del U_map
                 else:
                     PTNinv = al.partial_dot(pointing_mat.mat_transpose(), N.get_inverse())
                     map += al.dot(PTNinv, time_stream)
-                    print 'Process %d: ' % mpiutil.rank, map.shape
-                    print 'Process %d: ' % mpiutil.rank, map.info
+                    del time_stream
+                    
+                    print 'pointing_mat: ', pointing_mat.info
+                    print 'PTNinv: ', PTNinv.info
+                    PTNinvP = al.partial_dot(PTNinv, pointing_mat)
+                    del PTNinv
+                    del pointing_mat
+                    print 'Process %d: ' % mpiutil.rank, PTNinvP.shape
+                    print 'Process %d: ' % mpiutil.rank, PTNinvP.info
+                    # cov_inv += al.dot(PTNinv, pointing_mat)
                     
                 mpiutil.barrier()
                 err
                 # weighted_time_stream = N.weight_time_stream(time_stream)
-                map += P.apply_to_time_axis(weighted_time_stream)
+                # map += P.apply_to_time_axis(weighted_time_stream)
             if self.feedback > 1:
                 print "Dirty map done."
                 print "Building map covariance. Frequecies finished:"
